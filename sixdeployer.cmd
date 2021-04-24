a112e8e875f45ec4f37ed9de45e5e76a683fbc91
ytsix <39928244+ytsix@users.noreply.github.com>
2021-04-24 15:50:35 -0700

    Create sixdeployer.cmd

---
+++ sixdeployer.cmd
@@ -0,0 +1,130 @@
+@echo off
+title Six's Deployer \ Github.com/ytsix
+mode con cols=77 lines=25
+cls
+color f
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m          
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m          Welcome User         [47m                       
+echo.[43m                             [41m                   [43m                             
+ping localhost -n 2 >nul
+cls
+color f
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m     Made by Six github/ytsix     [47m                       
+echo.[43m                             [41m                   [43m                             
+ping localhost -n 2 >nul
+cls
+color f
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m   Press any key to continue   [47m                       
+echo.[43m                             [41m                   [43m                             
+pause >nul
+color 6
+cls
+:DEPLOY     
+echo Deploying... Please Wait...
+pip install random
+pip install string
+pip install threading
+pip install pypresence
+pip install colorama
+pip install itertools
+pip install tqdm
+pip install discord
+pip install dhooks
+pip install winreg
+pip install socket
+pip install pymysql
+pip install telnetlib
+ping localhost -n 10 >nul
+cls
+color f
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m   Thank You for Installing! Bye   [47m                       
+echo.[43m                             [41m                   [43m                             
+pause >nul
+
